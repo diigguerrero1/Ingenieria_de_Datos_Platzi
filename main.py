d3ec1777bac28596d99ad744c7caeeae9efb2db9
Diego Guerrero <diegoes9801@gmail.com>
2021-07-29 17:57:14 -0500

    Se termino el codigo del scrapper

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
 
+import news_page_objects as news
 from common import config
 
 logger = logging.getLogger(__name__)
@@ -9,7 +10,12 @@ logger = logging.getLogger(__name__)
 
 def _news_scraper(news_site_uid):
     host = config() ['news_sites'] [news_site_uid] ['url']
+
     logging.info('Beginning scraper for {}'.format(host))
+    homepage = news.HomePage(news_site_uid, host)
+
+    for link in homepage.article_links:
+        print(link)
 
 
 if __name__ == '__main__':
