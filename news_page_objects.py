7ed6b423a04634e6f7bfbc61868152cca8fd4d45
Diego Guerrero <diegoes9801@gmail.com>
2021-07-29 23:09:04 -0500

    nuevo commit de nueva instalación pip

--- news_page_objects.py
+++ news_page_objects.py
@@ -3,26 +3,16 @@ import requests
 
 from common import config
 
-class HomePage:
+class NewsPage:
 
-    def __init__(self, news_sites_uid, url):
-        self._config = config() ['news_sites'] [news_sites_uid]
+    def __init__(self, news_site_uid, url):
+        self._config = config() ['news_sites'] [news_site_uid]
         self._queries = self._config['queries']
         self._html = None
 
         self._visit(url)
 
-    @property
-    def article_links(self):
-        link_list = []
-        for link in self._select(self._queries['homepage_article_links']):
-            if link and link.has_attr('href'):
-                link_list.append
-
-        return set(link['href'] for link in link_list)
-
-
-    def _select(señf, query_string):
+    def _select(self, query_string):
         return self._html.select(query_string)
 
     def _visit(self, url):
@@ -31,4 +21,38 @@ class HomePage:
         response.raise_for_status()
 
         self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                link_list.append(link)
+
+        return set(link['href'] for link in link_list)
+
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
+
+
+
+
+    
 
